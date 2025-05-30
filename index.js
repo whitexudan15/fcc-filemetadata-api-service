82df65cdfbc5b43abcb99c0d47b2e29386fd6961
whitexudan15 <tresortakoudawhitexudan15@gmail.com>
2025-05-30 22:51:27 +0000

    GET /api/fileanalyse atteint

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+require("dotenv").config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+// Storage config
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
+// initialiser multer
+const upload = multer({ storage: storage });
 
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
+// GET /api/fileanalyse
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  res.json({
+    name: req.file.filename,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
+// Serveur
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
